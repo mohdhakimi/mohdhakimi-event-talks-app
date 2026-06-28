92d3f86d47f5866fcb10218a4bd18c4c122b71c4
Mohd Hakimi <MOHDHAKIMIMOHDNOR@GMAIL.COM>
2026-06-28 14:12:36 +0800

    Initial commit: 1-Day Technical Event Website with Node.js backend.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve the static index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
